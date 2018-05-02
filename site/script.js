ee955bf8906de70e561d46e223e028d2dfc5d808
matthias gaydu <gamatthias@gmail.com>
2018-05-02 11:29:14 +0200

    debut

---
+++ site/script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+		var donne = document.forms["myForm"]["donne"].value;
+		var pibpnb=document.forms["myForm"]["PIBPNB"].value;
+		var pays =document.forms["myForm"]["PAYS"].value;
+		var regime=document.forms["myForm"]["REGIME"].value;
+		var x =donne + "\n"+pibpnb+ "\n"+pays+"\n"+regime;
+		alert(x);
+		
+	
+		
+	}
+var djankovModel=
