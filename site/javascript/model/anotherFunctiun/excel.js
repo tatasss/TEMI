ce5569ccff0cec55ab16548f5fecffb14982e58a
matthias gaydu <gamatthias@gmail.com>
2018-05-24 17:42:47 +0200

    une erreur

--- site/javascript/model/anotherFunctiun/excel.js
+++ site/javascript/model/anotherFunctiun/excel.js
@@ -1,21 +1,21 @@
-function newApi(format, tab,titre) {
+function newApi(format, tab, titre) {
     console.log("on entre");
-    let args=tab.split(",");
-    let tabExcel=[];
-    let cpt=0;
+    let args = tab.split(",");
+    let tabExcel = [];
+    let cpt = 0;
     let regex1 = /^entreprise.*$/;
     tabExcel.push([]);
     tabExcel[0].push(" ");
-    for (let i=0;i<args.length;i++){
-        if(regex1.test(args[i])){
+    for (let i = 0; i < args.length; i++) {
+        if (regex1.test(args[i])) {
             tabExcel.push([]);
             cpt++;
         }
-        if(args[i]==="ligne"){
+        if (args[i] === "ligne") {
             tabExcel.push([]);
             cpt++;
         }
-        else{
+        else {
             tabExcel[cpt].push(args[i]);
         }
 
@@ -23,12 +23,12 @@ function newApi(format, tab,titre) {
     console.log(tabExcel);
     return ExcellentExport.convert({
         anchor: 'anchorNewApi-' + format,
-        filename: titre ,
+        filename: titre,
         format: format
     }, [{
         name: 'feuille TEMI',
         from: {
-            array:tabExcel
+            array: tabExcel
         }
     }]);
 }
