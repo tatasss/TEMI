790031836f6cd8794e81c997540398fe782e211b
tatasss <matth430@gmail.com>
2018-06-12 10:31:04 +0200

    premier

--- site/javascript/view/viewGenerator/bootstrapView.js
+++ site/javascript/view/viewGenerator/bootstrapView.js
@@ -312,7 +312,7 @@ BootstrapView.prototype.bootstrapTemiTabSpe = function (cote, head, tab, maMarge
     tabExcel.push("régime Fiscal");
     tabExcel.push(reg);
     html+=
-        `<a class="btn btn-default " download="${titre}.xls" href="#" id="anchorNewApi-xls" onClick="newApi('xls','${tabExcel.toString()}','${titre}')">Récupérer Excel</a>`;
+        `<button class="btn btn-default " download="${titre}.xls" id="anchorNewApi-xls" onClick="newApi('xls','${tabExcel.toString()}','${titre}')">Récupérer Excel</button>`;
     return html;
 };
 /**
